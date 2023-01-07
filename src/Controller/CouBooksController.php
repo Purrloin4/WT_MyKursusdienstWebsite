64a2ea54a8cb9dc5ab6eb9ed55420d562b678037
jeroen.vanaken <jeroen.vanaken@kuleuven.be>
2023-01-07 16:40:44 +0100

    added extra pages with template

--- src/Controller/CouBooksController.php
+++ src/Controller/CouBooksController.php
@@ -8,9 +8,48 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CouBooksController extends AbstractController {
 
+    private array $stylesheets;
+
+    public function __construct() {
+        $this->stylesheets[] = 'main.css';
+    }
+
     #[Route("/", name: "home")]
     public function home(): Response {
-        return $this->render('main.html.twig');
+        return $this->render('main.html.twig',[
+            'stylesheets'=> $this->stylesheets
+        ]);
+    }
+
+    #[Route("/courses", name: "courses")]
+    public function courses(): Response {
+        $this->stylesheets[] = 'courses.css';
+        return $this->render('courses.html.twig',[
+            'stylesheets'=> $this->stylesheets
+        ]);
+    }
+
+    #[Route("/reservation", name: "reservation")]
+    public function reservation(): Response {
+        $this->stylesheets[] = 'reservation.css';
+        return $this->render('reservation.html.twig',[
+            'stylesheets'=> $this->stylesheets
+        ]);
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response {
+        return $this->render('about.html.twig',[
+            'stylesheets'=> $this->stylesheets
+        ]);
+    }
+
+    #[Route("/feedback", name: "feedback")]
+    public function feedback(): Response {
+        $this->stylesheets[] = 'feedback.css';
+        return $this->render('feedback.html.twig',[
+            'stylesheets'=> $this->stylesheets
+        ]);
     }
 
 }
