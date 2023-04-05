a8d02c053c9e8041753dc8480203972058de7c77
Cédric <cedric.josse@student.kuleuven.be>
2023-04-06 00:16:29 +0200

    rest of the templates implemented

--- src/Controller/MyCouBooksController.php
+++ src/Controller/MyCouBooksController.php
@@ -22,4 +22,36 @@ class MyCouBooksController extends AbstractController
             'stylesheets' => $this->stylesheets,
         ]);
     }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig', [
+            'stylesheets' => $this->stylesheets,
+        ]);
+    }
+
+    #[Route('/courses', name: 'courses')]
+    public function courses(): Response
+    {
+        return $this->render('courses.html.twig', [
+            'stylesheets' => $this->stylesheets,
+        ]);
+    }
+
+    #[Route('/feedback', name: 'feedback')]
+    public function feedback(): Response
+    {
+        return $this->render('feedback.html.twig', [
+            'stylesheets' => $this->stylesheets,
+        ]);
+    }
+
+    #[Route('/reservation', name: 'reservation')]
+    public function reservation(): Response
+    {
+        return $this->render('reservation.html.twig', [
+            'stylesheets' => $this->stylesheets,
+        ]);
+    }
 }
